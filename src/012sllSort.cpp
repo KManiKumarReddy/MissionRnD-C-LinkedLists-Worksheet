d230ac413f4bafc67bdec66b4113659c80d18c47
KManiKumarReddy <KManiKumarReddy@live.com>
2015-11-16 02:02:38 +0530

    Submit Commit

--- src/012sllSort.cpp
+++ src/012sllSort.cpp
@@ -22,5 +22,25 @@ struct node {
 };
 
 void sll_012_sort(struct node *head){
-	
+	struct node * start = head;
+	// leaving heading 0s
+	while (start && !(start->data)) start = start->next;
+	if (!start) return;
+	// counting 0s, 1s and 2s
+	struct node * temp = start;
+	int count[3] = { 0, 0, 0 };
+	do count[temp->data] ++; while (temp = temp->next);
+	// changing data to 0s, 1s and 2s depending on their count
+	while (count[0]--) {
+		start->data = 0;
+		start = start->next;
+	}
+	while (count[1]--) {
+		start->data = 1;
+		start = start->next;
+	}
+	while (count[2]--) {
+		start->data = 2;
+		start = start->next;
+	}
 }

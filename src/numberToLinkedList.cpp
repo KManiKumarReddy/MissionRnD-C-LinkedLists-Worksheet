f81ec3b0f1b38873186e0902904ec14f843a3983
KManiKumarReddy <KManiKumarReddy@live.com>
2015-11-16 00:41:52 +0530

    Submit Commit

--- src/numberToLinkedList.cpp
+++ src/numberToLinkedList.cpp
@@ -20,5 +20,18 @@ struct node {
 };
 
 struct node * numberToLinkedList(int N) {
-	return NULL;
+	struct node * head = (struct node *)malloc(sizeof(struct node));
+	head->next = NULL;
+	if (N < 0)
+		N = -N;
+	head->num = N % 10;
+	N /= 10;
+	while (N) {
+		struct node * temp = (struct node *)malloc(sizeof(struct node));
+		temp->next = head;
+		head = temp;
+		head->num = N % 10;
+		N /= 10;
+	}
+	return head;
 }
